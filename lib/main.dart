e2891d289ca0f95b73516bcf1e1d47f4dfa86762
Tedros Teshome <tedrhanna@gmail.com>
2026-02-15 10:41:34 +0300

    feat: create circle avatar with gradient like insta and connect it's data from map, and get username from the map

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
 
         colorScheme: .fromSeed(seedColor: const Color.fromARGB(255, 255, 255, 255)),
@@ -39,10 +40,12 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   final List<Map<String, String>> userData = [
-    {"name": "Tedros", "image": "https://i.pravatar.cc/150?u=1"},
+    {"name": "Ruth", "image": "https://i.pravatar.cc/150?u=1"},
     {"name": "Moa", "image": "https://i.pravatar.cc/150?u=2"},
-    {"name": "Ezana", "image": "https://i.pravatar.cc/150?u=3"},
-  // Add as many as you like...
+    {"name": "I B", "image": "https://i.pravatar.cc/150?u=3"},
+    {"name": "Bruk", "image": "https://i.pravatar.cc/150?u=4"},
+    {"name": "Ezana", "image": "https://i.pravatar.cc/150?u=5"},
+  
   ];
 
   @override
@@ -57,41 +60,66 @@ class _MyHomePageState extends State<MyHomePage> {
           IconButton(onPressed: (){}, icon: Icon(Icons.messenger_outline)),
         ],
       ),
-      body: Column(
-        children:[
-          ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: 10,
-            
-            itemBuilder: (context, index) {
-              final user = userData[index];
-              return Container(
-                height: 100,
-                color: Colors.grey[300],
-                child: Column(
-                  children:[
-                    Row(
-                      children:[
-                        Container(
-                          width: 50,
-                          height: 50,
-                          margin: EdgeInsets.all(8),
+      body:Column(
+  children: [
+    SizedBox(
+      height: 110, // Adjusted height to fit text below circle
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: userData.length,
+        itemBuilder: (context, index) {
+          final user = userData[index];
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
+            child: Column(
+              children: [
+                Container(
+                  width: 65,
+                  height: 65,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    gradient: LinearGradient(
+                      colors: [Colors.yellow, Colors.red, Colors.purple],
+                      begin: Alignment.bottomLeft,
+                      end: Alignment.topRight,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(3.0), // The "border" thickness
+                    child: Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white, // Gap between ring and image
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: Container(
                           decoration: BoxDecoration(
                             shape: BoxShape.circle,
-                            color: Colors.blue,
+                            image: DecorationImage(
+                              image: NetworkImage(user['image']!),
+                              fit: BoxFit.cover,
+                            ),
                           ),
                         ),
-                        Text("User"),
-                      ],
+                      ),
                     ),
-                    
-                  ],
+                  ),
                 ),
-              );
-            },
-          ),
-        ],
+                SizedBox(height: 5),
+                Text(
+                  user['name']!,
+                  style: TextStyle(fontSize: 12),
+                ),
+              ],
+            ),
+          );
+        },
       ),
+    ),
+     
+  ],
+),
     );
   }
 }
