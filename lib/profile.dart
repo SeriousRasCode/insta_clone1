58e56e99b350b8754ad0958fdffd143c77a29b25
Tedros Teshome <tedrhanna@gmail.com>
2026-02-16 18:12:57 +0300

    feat: define profileData Map

---
+++ lib/profile.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  final Map<String, dynamic> profileData = const {
+    "username": "seriousras",
+    "name": "Tedros Teshome",
+    "bio": "Artist | Developer",
+    "description": "የአንበሳው መንፈስ ከኔጋራ ነው፤",
+    "link": "github.com/SeriousRasCode",
+    "posts": 8,
+    "followers": 335,
+    "following": 435,
+    "profilePic": "https://i.pravatar.cc/150?u=2",
+  };
