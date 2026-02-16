bf118bb893b836db0ba909d755fb450eaaf7bf92
Tedros Teshome <tedrhanna@gmail.com>
2026-02-16 18:13:54 +0300

    feat: create the insta appBar design clone

--- lib/profile.dart
+++ lib/profile.dart
@@ -14,3 +14,32 @@ class ProfilePage extends StatelessWidget {
     "following": 435,
     "profilePic": "https://i.pravatar.cc/150?u=2",
   };
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: false,
+        title: Row(
+          children: [
+            Text(
+              profileData["username"],
+              style: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+            ),
+            const Icon(Icons.keyboard_arrow_down, color: Colors.black),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add_box_outlined, color: Colors.black),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.menu, color: Colors.black),
+          ),
+        ],
+      ),
